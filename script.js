97dcdf2e0489c7640b0b05112f7593327d902fa0
Alexandre Macena <alexandremacena@prof.educacao.sp.gov.br>
2025-09-01 17:44:22 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ document.addEventListener('DOMContentLoaded', function(){
   
  })
 
- ScrollReveal().reveal('#inicio', { delay: 500 });
- ScrollReveal().reveal('#tropicalia', { delay: 500 });
- ScrollReveal().reveal('#galeria', { delay: 500 });
- ScrollReveal().reveal('#contato', { delay: 500 });
+ ScrollReveal().reveal('#inicio', { delay: 600 });
+ ScrollReveal().reveal('#empresasocial', { delay: 600 });
+ ScrollReveal().reveal('#galeria', { delay: 600 });
+ ScrollReveal().reveal('#contato', { delay: 600 });
